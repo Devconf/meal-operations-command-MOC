26b9d4bad7723d79b2d3758f40563db12df85c25
kim hyun sik <wmf2fkrh@gmail.com>
2020-07-05 19:19:22 +0900

    Fix json format

--- add_nut.py
+++ add_nut.py
@@ -2,21 +2,29 @@ import json
 
 def add_nutrient(year,menu_json):
     annual_diet=[]
-    month_diet={}
+   
     
     for month in range(1,13):
+        month_diet={}
         with open(f'./diet_gen/{year}년{month}월_식단.json', 'r', encoding='utf-8') as f:
             month_meal= json.loads(f.read().replace("\n",""))
             f.close()
+            month_diet["year"]=year
+            month_diet["month"]=str(month)
+            month_diet["meals"]=[]
             for key,val in month_meal.items():
-                new_menu={}
-                for k,v in val.items():
-                    for menu in v:
+                meal={}
+                meal["day"]=key
+                for k,v in val.items(): #val.k=breakfast
+                    meal[k]=[]
+                    #new_menu={}
+                    for menu in v: #v=food[]
                         for m_k,m_v in menu_json.items():
                             if menu ==m_k:
-                                new_menu[menu]=m_v
+                                m_v["title"]=menu
+                                meal[k].append(m_v)
                                 break
-                    val[k]=new_menu
-            month_diet[month]=month_meal
-    annual_diet.append(month_diet)
+                    #val[k]=new_menu   
+                month_diet["meals"].append(meal) 
+        annual_diet.append(month_diet)
     return annual_diet
